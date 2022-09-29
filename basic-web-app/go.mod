4157282ffe18e363853b7a28307ba8a143283f8b
kumarshubhampratikrauniyar <shubhamrauniyar10@gmail.com>
2022-09-29 19:42:26 +0545

    basic web app

---
+++ basic-web-app/go.mod
@@ -0,0 +1,3 @@
+module basic-web-app
+
+go 1.19
---
+++ basic-web-app/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the homepage handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+
+	fmt.Fprintf(w, fmt.Sprintf("This is the about page of 2+2 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	http.ListenAndServe(portNumber, nil)
+}
