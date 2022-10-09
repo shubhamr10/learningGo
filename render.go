7e24fa4d160d71e339e15f8d1147285edafdae34
kumarshubhampratikrauniyar <shubhamrauniyar10@gmail.com>
2022-10-09 19:55:02 +0545

    Basic server templating using .tmpl

---
+++ render.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error while parsing template", err)
+		return
+	}
+}
