4757c4815f6aa6f4e375c45b99519083033b6ec9
kumarshubhampratikrauniyar <shubhamrauniyar10@gmail.com>
2022-12-22 13:35:38 +0530

    Added command line arguments

---
+++ migrations/20221221152546_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO users
+( first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES( 'Shubham', 'Rauniyar', 'admin@admin.co', '$2a$12$ktzr4BlXDG/tRkQRCu2AMui3oXp15pGztL9G1yTkx3IBKt/U6EWr6', 3, '2022-12-09 00:00:00.000', '2022-12-09 00:00:00.000');
